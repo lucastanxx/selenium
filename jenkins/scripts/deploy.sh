e2d3e3891c9c9e15cae08442f47376d3c31209e1
Ambulance <32918823+lucastanxx@users.noreply.github.com>
2024-07-18 17:03:39 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,35 @@
 #!/usr/bin/env sh
 
+# Enable command echo
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
-sleep 1
+
+# Set variables
+PORT=80
+HOSTNAME="localhost"
+CONTAINER_NAME="my-apache-php-app"
+
+# Run Docker container
+docker run -d -p ${PORT}:${PORT} --name ${CONTAINER_NAME} -v c:/path/to/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+
+# Disable command echo
 set +x
 
-echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
+# Give some time for the container to start
+sleep 5
+
+# Define the URL
+URL="http://${HOSTNAME}:${PORT}"
+
+# Print URL and port
+echo "Now..."
+echo "Visit ${URL} to see your PHP application in action."
+
+# Ping the application to check for a 200 OK response
+RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" "${URL}")
+
+if [ "$RESPONSE" -eq 200 ]; then
+    echo "Success: Server responded with HTTP 200."
+else
+    echo "Error: Server response was HTTP $RESPONSE."
+fi
 
