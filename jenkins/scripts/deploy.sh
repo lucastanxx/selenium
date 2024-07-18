25c9d1a96ce3e70d79338f18dc9602e0b4625556
Ambulance <32918823+lucastanxx@users.noreply.github.com>
2024-07-18 17:35:57 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -7,7 +7,7 @@ set -x
 PORT=80
 HOSTNAME=localhost
 CONTAINER_NAME=my-apache-php-app
-SOURCE_DIR=/c/Users/lucas/Desktop/SSD/Jenkinsv2/jenkins-php-selenium-test/src
+SOURCE_DIR="/c/Users/lucas/Desktop/SSD/Jenkinsv2/jenkins-php-selenium-test/src"
 
 # Check if the container already exists and remove it if it does
 docker ps -a | grep $CONTAINER_NAME && docker rm -f $CONTAINER_NAME
@@ -15,12 +15,18 @@ docker ps -a | grep $CONTAINER_NAME && docker rm -f $CONTAINER_NAME
 # Run Docker container
 docker run -d -p ${PORT}:${PORT} --name $CONTAINER_NAME -v $SOURCE_DIR:/var/www/html php:7.2-apache 2>&1
 
-# Sleep for 1 second to allow the container to start
-sleep 1
-docker ps -a
-docker inspect my-apache-php-app
-docker exec my-apache-php-app ps aux | grep apache
+# Sleep for a few seconds to allow the container to start
+sleep 5
 
+# Display Docker container logs
+docker logs $CONTAINER_NAME
+
+# Display Apache process status
+docker exec $CONTAINER_NAME ps aux | grep apache
+
+# Display Apache error and access logs
+docker exec $CONTAINER_NAME cat /var/log/apache2/error.log
+docker exec $CONTAINER_NAME cat /var/log/apache2/access.log
 
 # Check if the container is running
 if ! docker ps | grep $CONTAINER_NAME; then
@@ -36,7 +42,6 @@ else
     echo "Failed to reach application, HTTP status: $HTTP_STATUS"
     exit 2
 fi
-
 
 # Disable debug mode
 set +x
