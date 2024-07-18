0eea072b0703de05b0075fb2f7c440e2208fd623
Ambulance <32918823+lucastanxx@users.noreply.github.com>
2024-07-18 17:09:43 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,35 +1,20 @@
 #!/usr/bin/env sh
 
-# Enable command echo
+# Enable script debugging
 set -x
 
-# Set variables
-PORT=80
-HOSTNAME="localhost"
-CONTAINER_NAME="my-apache-php-app"
-
 # Run Docker container
-docker run -d -p ${PORT}:${PORT} --name ${CONTAINER_NAME} -v C:\\Users\\lucas\\Desktop\\SSD\\Jenkinsv2\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\lucas\\Desktop\\SSD\\Jenkinsv2\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 
-# Disable command echo
+# Pause to allow some time for the container to start
+sleep 1
+
+# Perform a curl request to check if the server returns HTTP 200
+curl -s -o /dev/null -w "%{http_code}" http://localhost | grep 200 && echo "Application is up and running" || echo "Failed to reach application"
+
+# Disable script debugging
 set +x
 
-# Give some time for the container to start
-sleep 5
-
-# Define the URL
-URL="http://${HOSTNAME}:${PORT}"
-
-# Print URL and port
-echo "Now..."
-echo "Visit ${URL} to see your PHP application in action."
-
-# Ping the application to check for a 200 OK response
-RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" "${URL}")
-
-if [ "$RESPONSE" -eq 200 ]; then
-    echo "Success: Server responded with HTTP 200."
-else
-    echo "Error: Server response was HTTP $RESPONSE."
-fi
-
+# Informative message about the application
+echo 'Now...'
+echo 'Visit http://localhost to see your PHP application in action.'
